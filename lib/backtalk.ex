b35059f3e98c228d7c17ee819d0d38d11e43d0c2
Robert Lord <robert@lord.io>
2015-08-19 23:50:28 -0400

    Add more router

--- lib/backtalk.ex
+++ lib/backtalk.ex
@@ -4,7 +4,18 @@ defmodule Backtalk do
       def init(options), do: options
 
       def call(conn, _opts) do
-        result = conn |> route(method, conn.path_info)
+        case route(conn, method, conn.path_info) do
+          body when is_string(body) -> set_reply(conn, 200, %{}, body)
+          code when is_number(code) -> set_reply(conn, code, %{}, "")
+          {code, body} when is_number(code) and is_string(body) -> set_reply(conn, code, %{}, body)
+          {code, headers, body} when is_number(code) and is_string(body) and is_map(headers) -> set_reply(conn, code, headers, body)
+          new_conn -> new_conn # TODO can check if matches conn struct type?
+          # TODO body can be not a string, can be a symbol and then we match as appropriate
+        end
+      end
+
+      defp set_reply(conn, code, headers, body) do
+        # TODO
       end
 
       defp send(conn) do
