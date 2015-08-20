199b10224608a98d03409a78d908d4b8da03b27b
Robert Lord <robert@lord.io>
2015-08-19 23:44:05 -0400

    Add start of router

--- lib/backtalk.ex
+++ lib/backtalk.ex
@@ -1,2 +1,19 @@
 defmodule Backtalk do
+  defmacro __using__(_opts) do
+    quote do
+      def init(options), do: options
+
+      def call(conn, _opts) do
+        result = conn |> route(method, conn.path_info)
+      end
+
+      defp send(conn) do
+        if conn.state == :set do
+          Plug.Conn.send_resp(conn)
+        else
+          conn
+        end
+      end
+    end
+  end
 end
